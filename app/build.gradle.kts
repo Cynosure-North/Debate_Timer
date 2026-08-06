254dd152244765005785b51d564f23341e466132
North <cynosure.north@gmail.com>
2026-08-07 11:36:08 +1200

    Bump version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "page.cynosure.timer"
         minSdk = 35
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
